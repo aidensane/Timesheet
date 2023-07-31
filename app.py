25146fd78df655570508bde51a93646d296c9837
Aiden Samuels <aide3ndev@gmail.com>
2023-07-31 09:08:02 +0100

    fixed gitignore

--- app.py
+++ app.py
@@ -13,10 +13,20 @@ def index():
 def add_time_entry():
     try:
         date = request.form["date"]
-        date = date.validate_date(date)
+        date = validation.validate_date(date)
 
         start_time = request.form["start_time"]
-        start_time
+        start_time = validation.validate_time(start_time)
+
+        end_time = request.form["end_time"]
+        end_time = validation.validate_time(end_time)
+
+        task = request.form["task"]
+        task = validation.validate_task(task)
+
+        timesheet.add_time_entry_date(date, start_time.strftime("%H:%H"), end_time.strftime("%H:%H"), task)
+        
+
 
     except ValueError as e:
         return
